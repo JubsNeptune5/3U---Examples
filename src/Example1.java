e8e88df4438e144aa6cda78b70866bf8640dda16
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-02-09 10:00:50 -0500

    Initial commit. First Example added

---
+++ src/Example1.java
@@ -0,0 +1,29 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author laveh2107
+ */
+public class Example1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Creating a new city called kitchener
+        City kitchener = new City();
+        
+        //Creating a robot
+        Robot taylor = new Robot(kitchener, 2, 1, Direction.EAST);
+        taylor.move();
+        taylor.turnLeft();
+    }
+}

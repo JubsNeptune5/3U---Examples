02a80f16c2d57c88e893163b2528ae7e746e2b64
laveh2107 <laveh2107@HRH-IC0027329.wrdsb.ca>
2017-02-21 10:05:55 -0500

    Initial Commit

--- src/Countedloops.java
+++ src/Countedloops.java
@@ -25,7 +25,7 @@ public class Countedloops {
 
         // make a counter
         int counter = 0;
-        while(counter<5){
+        while (counter < 5) {
             //move
             joe.move();
             //increase counter
@@ -34,11 +34,30 @@ public class Countedloops {
             //counter +=1; <-- another shortcut
         }
         joe.turnAround();
-        
+
         //using a for loop to count
         //for(define a variable; condition to loop; increase variable)
-        for(int i=0; i<5; i = i +1){
-        joe.move();
-    }
+        for (int i = 0; i < 5; i = i + 1) {
+            joe.move();
+        }
+        //Coumpound Condition
+        //&& <- AND statement - both must be true
+        //|| <- OR statement - one of them must be true
+        if (joe.frontIsClear()) {
+            if (joe.countThingsInBackpack() == 5) {
+                //do something
+            }
+        }
+        if (joe.frontIsClear() && joe.countThingsInBackpack() == 5) {
+            //do something
+        }
+        if (joe.frontIsClear()) {
+            //do something
+        } else if (joe.countThingsInBackpack() == 5) {
+            //do same thing as above if statement
+        }
+        if (joe.frontIsClear() || joe.countThingsInBackpack() == 5) {
+            //do something
+        }
     }
 }
